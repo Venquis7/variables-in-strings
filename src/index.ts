5841d58c9a6535e8385edc9160f65b75e06e7ea9
Venquis7 <tpajor_public@icloud.com>
2024-10-11 18:26:19 +0200

    First commit

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+function getObjectProp<K extends keyof object>(obj: object, key: string) {
+	return obj[key as K];
+}
+
+export default function MagicString(str: string, data?: object): string {
+	let matches = str?.match(/%(\S+?)%/gim) || [];
+
+	if (data && matches.length > 0) {
+		matches.forEach((currentMatch) => {
+			let extract = currentMatch.replace(/%/g, '');
+			let props = extract.split('.');
+			let current: object = data || {};
+
+			for (let prop of props) {
+				const currentObject = current[prop as keyof object];
+
+				if (current) {
+					if (currentObject && typeof currentObject === 'object') {
+						current = getObjectProp(current, prop);
+						continue;
+					} else {
+						let value: string = currentObject;
+
+						if (value !== undefined) {
+							str = str.replace(currentMatch, value);
+							console.log(currentMatch);
+						}
+
+						break;
+					}
+				}
+			}
+		});
+	}
+
+	return str;
+}
